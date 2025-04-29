03d6862b3659af7191e4439ed0f93b13fcd26e8a
Juli881 <julietarohr07@gmail.com>
2025-04-29 11:04:50 -0300

    paginagatitos

--- src/app/Shared/navbar/navbar.component.ts
+++ src/app/Shared/navbar/navbar.component.ts
@@ -1,25 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { producto } from '../../model/producto.model';
+import { CarritoService } from '../../servicios/carrito.service';
 
 @Component({
   selector: 'app-navbar',
-  standalone:true,
-  imports: [RouterLink,CommonModule,RouterModule],
+  standalone: true,
+  imports: [RouterLink, CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'] 
 })
-export class NavbarComponent implements OnInit{
-cantidadProductos:number = 0;
-constructor(private carritoservice: CarritoService){}
+export class NavbarComponent implements OnInit {
+  cantidadProductos: number = 0;
 
-ngOnInit(): void {  
-  //Escucha los cambios en el carrito para actualizar la cantidad de productos
-this.carritoservice.carrito$.subscribe((productos: { Producto: Producto, cantidad: number}[])=>{
-this.cantidadProductos = productos.reduce((total, item) => total + item.cantidad,0)
-})
+  constructor(private carritoservice: CarritoService) {}
+
+  ngOnInit(): void {
+      //Escucha los cambios en el carrito para actualizar la cantidad de productos
+    this.carritoservice.carrito$.subscribe((productos: { producto: producto, cantidad: number }[]) => {
+      this.cantidadProductos = productos.reduce((total, item) => total + item.cantidad, 0);
+    });
+  }
+
+  onCarritoClick() {
+    console.log('carrito clicked');
+  }
 }
-onCarritoClick(){
-  console.log('carrito clicked')
-}
-}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, NavbarComponent, RouterModule,FooterComponent], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'ProyectoPracticas';
